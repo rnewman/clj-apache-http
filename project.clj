1eeda5c27813113ac2b9e75e1a7e52b7a43c4320
Richard Newman <rnewman@twinql.com>
2010-01-18 22:41:45 -0800

    Bump version to reflect new, backwards-compatible changes.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twinql.clojure/clj-apache-http "1.0"
+(defproject com.twinql.clojure/clj-apache-http "1.1.0"
  :description "Clojure HTTP library using the Apache HttpClient."
  :dependencies [[org.apache.httpcomponents/httpcore "4.0.1"]
                 [org.apache.httpcomponents/httpmime "4.0.1"]
