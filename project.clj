595105a4299cd26d214a219d9173579f1f8104f7
Richard Newman <rnewman@twinql.com>
2010-01-22 19:01:11 -0800

    Updated deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject com.twinql.clojure/clj-apache-http "1.1.0"
  :description "Clojure HTTP library using the Apache HttpClient."
- :dependencies [[org.apache.httpcomponents/httpcore "4.0.1"]
+ :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+                [org.apache.httpcomponents/httpcore "4.0.1"]
                 [org.apache.httpcomponents/httpmime "4.0.1"]
                 [commons-logging/commons-logging "1.1.1"]
                 [org.apache.httpcomponents/httpclient "4.0.1"]])
