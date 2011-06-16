27af7efcee68fd715d3ef63c01cf352af0e2a48c
A. Diamond <andrew@hotelicopter.com>
2011-06-16 09:49:54 -0400

    Version 1.0.5

--- make-clojar.sh
+++ make-clojar.sh
@@ -1,6 +1,6 @@
 lein compile com.twinql.clojure.NaiveTrustManager
 lein compile com.twinql.clojure.PermissiveHostnameVerifier
-lein jar clj-apache-https-1.0.4.jar
-jar uf clj-apache-https-1.0.4.jar classes/com/twinql/clojure/*
+lein jar clj-apache-https-1.0.5.jar
+jar uf clj-apache-https-1.0.5.jar classes/com/twinql/clojure/*
 lein pom
-echo 'scp pom.xml clj-apache-https-1.0.4.jar clojars@clojars.org:'
+echo 'scp pom.xml clj-apache-https-1.0.5.jar clojars@clojars.org:'
