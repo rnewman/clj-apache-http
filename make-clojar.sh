abf438ec151e9c0cc8ef633ff27d3bf7bdbb5a8f
A. Diamond <andrew@hotelicopter.com>
2011-10-11 10:49:34 -0400

    Bumped version to 1.0.16.

--- make-clojar.sh
+++ make-clojar.sh
@@ -1,5 +1,5 @@
 lein compile com.twinql.clojure.NaiveTrustManager
-lein jar clj-apache-https-1.0.14.jar
-jar uf clj-apache-https-1.0.14.jar classes/com/twinql/clojure/*
+lein jar clj-apache-https-1.0.16.jar
+jar uf clj-apache-https-1.0.16.jar classes/com/twinql/clojure/*
 lein pom
-echo 'scp pom.xml clj-apache-https-1.0.14.jar clojars@clojars.org:'
+echo 'scp pom.xml clj-apache-https-1.0.16.jar clojars@clojars.org:'
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diamondap/clj-apache-https "1.0.15-SNAPSHOT"
+(defproject diamondap/clj-apache-https "1.0.16"
   :description "Clojure HTTP library using the Apache HttpClient. Based on clj-apache-http, but includes support for SSL client certificates and HttpAsyncClient."
 
   :dependencies [[org.clojure/clojure "1.2.1"]
@@ -9,7 +9,7 @@
                  [org.apache.httpcomponents/httpclient "4.1.1"]
                  [org.apache.httpcomponents/httpasyncclient "4.0-alpha2"]
                  [commons-codec "1.5"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]] 
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :repositories {"releases" ~(str "file://"
                                   user/local-maven-clone "/releases")
                  "snapshots" ~(str "file://"
