2b7274061fce6500a4faf8fc6ac1c293f73d1c68
A. Diamond <andrew@hotelicopter.com>
2011-11-03 13:20:02 -0400

    Changed version of Apache async client lib from 4.0-alpha2 to 4.0-alpha3

--- make-clojar.sh
+++ make-clojar.sh
@@ -1,5 +1,5 @@
 lein compile com.twinql.clojure.NaiveTrustManager
-lein jar clj-apache-https-1.0.16.jar
-jar uf clj-apache-https-1.0.16.jar classes/com/twinql/clojure/*
+lein jar clj-apache-https-1.0.17.jar
+jar uf clj-apache-https-1.0.17.jar classes/com/twinql/clojure/*
 lein pom
-echo 'scp pom.xml clj-apache-https-1.0.16.jar clojars@clojars.org:'
+echo 'scp pom.xml clj-apache-https-1.0.17.jar clojars@clojars.org:'
