28d57c9ff9d7827ec10ded5a9fe94c38a0772e24
A. Diamond <andrew@hotelicopter.com>
2011-11-03 13:56:47 -0400

    Rolled back last set of changes. 1.0.18 is now the same as 1.0.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diamondap/clj-apache-https "1.0.17"
+(defproject diamondap/clj-apache-https "1.0.18"
   :description "Clojure HTTP library using the Apache HttpClient. Based on clj-apache-http, but includes support for SSL client certificates and HttpAsyncClient."
 
   :dependencies [[org.clojure/clojure "1.2.1"]
@@ -7,7 +7,7 @@
                  [org.apache.httpcomponents/httpmime "4.1.1"]
                  [commons-logging/commons-logging "1.1.1"]
                  [org.apache.httpcomponents/httpclient "4.1.1"]
-                 [org.apache.httpcomponents/httpasyncclient "4.0-alpha3"]
+                 [org.apache.httpcomponents/httpasyncclient "4.0-alpha2"]
                  [commons-codec "1.5"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :repositories {"releases" ~(str "file://"
