3d3de1da943ec7dfac5c7c419f5a31efdbf368ff
Roman Gonzalez <romanandreg@gmail.com>
2012-01-07 18:47:27 -0800

    Removing lein-swank as a dev-dependency
    Technomancy suggests in the README of leiningen to not put
    lein-swank in the projects dev-dependencies but instead install
    this via the `lein plugin` utility.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject com.twinql.clojure/clj-apache-http "2.3.1"
  :description "Clojure HTTP library using the Apache HttpClient."
- :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
  :dependencies [[org.clojure/clojure "1.2.0"]
                 [org.clojure/clojure-contrib "1.2.0"]
                 [org.apache.httpcomponents/httpcore "4.0.1"]
