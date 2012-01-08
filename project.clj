5c0c24f56be761fa4c139cdab82d2a5dff03d027
Roman Gonzalez <romanandreg@gmail.com>
2012-01-07 19:57:42 -0800

    Updating library to clojure version 1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.twinql.clojure/clj-apache-http "2.3.1"
  :description "Clojure HTTP library using the Apache HttpClient."
- :dependencies [[org.clojure/clojure "1.2.0"]
-                [org.clojure/clojure-contrib "1.2.0"]
+ :dependencies [[org.clojure/clojure "1.3.0"]
+                [org.clojure/data.json "0.1.2"]
                 [org.apache.httpcomponents/httpcore "4.0.1"]
                 [org.apache.httpcomponents/httpmime "4.0.1"]
                 [commons-logging/commons-logging "1.1.1"]
